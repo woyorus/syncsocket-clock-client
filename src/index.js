302688dd3e369c1998c5ff1066f7e0768bc869af
Kirill Kravinsky <woyorus@gmail.com>
2016-07-31 15:48:13 -0400

    initial algorithm implementation

--- src/index.js
+++ src/index.js
@@ -9,10 +9,55 @@ function ClockClient(url, opts) {
     let parsedUrl = URL.parse(url);
     this.serverHost = parsedUrl.hostname;
     this.serverPort = parseInt(parsedUrl.port) || 5579;
+    this.targetPrecision = opts.targetPrecision || 50;
+    this.minReadingDelay = opts.minReadingDelay || 1;
+    this.clockDrift = opts.clockDrift || 0;
+
+    let upperBound = this.calcUpperBound();
+    let minUpperBound = this.calcMinUpperBound();
+
+    if (!(this.verifyUpperBound(upperBound, minUpperBound))) {
+        throwInvalidSetting();
+    }
+
+    this.timeoutDelay = this.calcTimeoutDelay(upperBound);
+
+    console.log(`timeoutDelay=${this.timeoutDelay}, minUB=${minUpperBound}. Adjust settings to make these as close as possible`);
 }
 
-ClockClient.prototype.sendClock = function (cb) {
-    let localStampSent = Date.now();
+function throwInvalidSetting() {
+    throw new Error('Clock client is set up incorrectly. Check client parameters.');
+}
+
+ClockClient.prototype.sync = function () {
+    return new Promise((resolve, reject) => {
+        let localSentStamp = Date.now();
+        this.sendClock(localSentStamp, (err, remoteResponseStamp) => {
+            let localRecvStamp = Date.now();
+
+            if (err) {
+                throw err;
+            }
+
+            let halfRound = this.calcHalfRoundTrip(localSentStamp, localRecvStamp);
+
+            let readingResult = {
+                error: this.calculateReadError(halfRound),
+                adjust: this.calculateAdjust(halfRound, remoteResponseStamp, localRecvStamp),
+                successful: this.isReadingSuccessful(halfRound)
+            };
+
+            if (readingResult.successful === true) {
+                resolve(readingResult);
+            } else {
+                reject(readingResult);
+            }
+        });
+    });
+};
+
+ClockClient.prototype.sendClock = function (clock, cb) {
+    let localStampSent = clock;
     let request = http.get({
         host: this.serverHost,
         port: this.serverPort,
@@ -27,7 +72,6 @@ ClockClient.prototype.sendClock = function (cb) {
         var body = '';
         res.on('data', (d) => body += d);
         res.on('end', () => {
-            let localStampReceived = Date.now();
             let parts = body.split(',');
             let responseStampCheck = parts[0];
             let responseStampRemote = parts[1];
@@ -35,11 +79,7 @@ ClockClient.prototype.sendClock = function (cb) {
                 cb(new Error('Timestamp verification failed!'));
             }
             else {
-                cb(null, {
-                    sent: localStampSent,
-                    received: localStampReceived,
-                    remote: parseInt(responseStampRemote)
-                });
+                cb(null, parseInt(responseStampRemote));
             }
         });
     });
@@ -49,3 +89,37 @@ ClockClient.prototype.sendClock = function (cb) {
     request.end();
 };
 
+ClockClient.prototype.calcHalfRoundTrip = function (stampSent, stampReceived) {
+    return (stampReceived - stampSent) / 2;
+};
+
+ClockClient.prototype.calcUpperBound = function () {
+    return (1 - (2 * this.clockDrift)) * (this.targetPrecision + this.minReadingDelay);
+};
+
+ClockClient.prototype.calcMinUpperBound = function () {
+    return this.minReadingDelay * (1 + this.clockDrift);
+};
+
+ClockClient.prototype.calcTimeoutDelay = function (upperBound) {
+    return 2 * upperBound;
+};
+
+ClockClient.prototype.verifyUpperBound = function (upperBound, minUpperBound) {
+    return upperBound >= minUpperBound;
+};
+
+ClockClient.prototype.isReadingSuccessful = function (halfRound) {
+    return (2 * halfRound) <= this.timeoutDelay;
+};
+
+ClockClient.prototype.calculateAdjust = function (halfRound, remoteTimestamp, localRecvTimestamp) {
+    return (remoteTimestamp + halfRound) - localRecvTimestamp;
+};
+
+ClockClient.prototype.calculateReadError = function (halfRound) {
+    let e = halfRound * (1 + (2 * this.clockDrift)) - this.minReadingDelay;
+    let eMin = 3 * this.clockDrift * this.minReadingDelay;
+    if (e < eMin) throw new Error('Assertion failed: e < eMin');
+    return e;
+};
--- test/index.js
+++ test/index.js
@@ -25,34 +25,149 @@ describe('ClockClient', function () {
         expect(cli.serverPort).to.be.equal(testingPort);
     });
 
-    describe('#sendClock()', function () {
+    const mockTargetPrecision = 15,
+        mockMinReadingDelay = 5,
+        mockClockDrift = 0.003;
+
+    it('should use provided options instead of defaults', function () {
+        let cli = Client(mockServerUrl, {
+            targetPrecision: mockTargetPrecision,
+            minReadingDelay: mockMinReadingDelay,
+            clockDrift: mockClockDrift,
+        });
+        expect(cli.targetPrecision).to.be.equal(mockTargetPrecision);
+        expect(cli.minReadingDelay).to.be.equal(mockMinReadingDelay);
+        expect(cli.clockDrift).to.be.eql(mockClockDrift);
+    });
+
+    it('should correctly calculate timeout delay', function () {
+        let cli = Client(mockServerUrl, {
+            targetPrecision: mockTargetPrecision,
+            minReadingDelay: mockMinReadingDelay,
+            clockDrift: mockClockDrift,
+        });
+        expect(cli.timeoutDelay).to.be.eql(39.76);
+    });
+
+    describe('#verifyUpperBound(upperBound, minUpperBound)', function () {
+        it('should be true if upper bound greater or equal to min upper bound', function () {
+            let cli = Client(mockServerUrl, {
+                targetPrecision: mockTargetPrecision,
+                minReadingDelay: mockMinReadingDelay,
+                clockDrift: mockClockDrift,
+            });
+            expect(cli.verifyUpperBound(3, 2)).to.be.true;
+            expect(cli.verifyUpperBound(2, 2)).to.be.true;
+            expect(cli.verifyUpperBound(1, 2)).to.be.false;
+        });
+    });
+
+    describe('#calcUpperBound()', function () {
+        it('should be [(1-(2*clockDrift)) * (targetPrecision + minReadingDelay)]', function () {
+            let cli = Client(mockServerUrl, {
+                targetPrecision: mockTargetPrecision,
+                minReadingDelay: mockMinReadingDelay,
+                clockDrift: mockClockDrift,
+            });
+            expect(cli.calcUpperBound()).to.be.eql(
+                (1-(2*mockClockDrift)) * (mockTargetPrecision + mockMinReadingDelay)
+            );
+        })
+    });
+
+    describe('#mimimumUpperBound()', function () {
+        it('should be minReadingDelay * (1 + clockDrift)', function () {
+            let cli = Client(mockServerUrl, {
+                targetPrecision: mockTargetPrecision,
+                minReadingDelay: mockMinReadingDelay,
+                clockDrift: mockClockDrift,
+            });
+            expect(cli.calcMinUpperBound()).to.be.eql(mockMinReadingDelay * (1 + mockClockDrift));
+        })
+    });
+
+    describe('#calcTimeoutDelay(upperBound)', function () {
+        it('should be twice the upperBound', function () {
+            let cli = Client(mockServerUrl);
+            expect(cli.calcTimeoutDelay(5)).to.be.eql(10);
+            expect(cli.calcTimeoutDelay(15)).to.be.eql(30);
+        });
+    });
+
+    describe('#isReadingSuccessful(halfRound)', function () {
+        it('should be false when 2*halfRound > timeoutDelay', function () {
+            let cli = Client(mockServerUrl, {
+                targetPrecision: mockTargetPrecision,
+                minReadingDelay: mockMinReadingDelay,
+                clockDrift: mockClockDrift,
+            });
+            expect(cli.isReadingSuccessful((cli.timeoutDelay + 1) / 2)).to.be.false;
+            expect(cli.isReadingSuccessful((cli.timeoutDelay - 1) / 2)).to.be.true;
+        });
+    });
+
+    describe('#calculateAdjust(halfRound, remoteTimestamp, localRecvTimestamp)', function () {
+        it('should return (remoteTimestamp + halfRound) - localRecvTimestamp', function () {
+            let cli = Client(mockServerUrl, {
+                targetPrecision: mockTargetPrecision,
+                minReadingDelay: mockMinReadingDelay,
+                clockDrift: mockClockDrift,
+            });
+
+            expect(cli.calculateAdjust(10, 1469993341000, 1469993343000)).to.be.eql(-1990);
+        })
+    });
+
+
+    describe("#sync()", function () {
+
+        it('should return promise', function () {
+            let cli = Client(mockServerUrl);
+            expect(cli.sync()).to.be.a('promise');
+        });
+
+    });
+
+    describe('#calcHalfRoundTrip(sent, received)', function () {
+        it('should return half of range between sent and received time', function () {
+            let cli = Client(mockServerUrl);
+            let mockTimeSent = 1469985602000,
+                mockTimeRecv = 1469985606000;
+            let range = mockTimeRecv - mockTimeSent;
+            let half = range / 2;
+            expect(cli.calcHalfRoundTrip(mockTimeSent, mockTimeRecv)).to.be.equal(half);
+        });
+    });
+
+    describe('#sendClock(cb)', function () {
+        var testBeginStamp;
+
+        beforeEach(function () {
+            testBeginStamp = Date.now();
+        });
 
         it('should send local and receive remote timestamp', function (done) {
             let cli = Client(mockServerUrl);
-            let testBeginStamp = Date.now();
-            let responseStamp = 1469978865700;
+            let mockResponseClock = 1469978865700;
             nock(mockServerUrl)
                 .get('/')
                 .socketDelay(100)
                 .reply(function (uri, requestBody, cb) {
                     let clientStamp = this.req.headers['x-client-timestamp'];
-                    cb(null, [200, clientStamp + ',' + responseStamp]);
+                    cb(null, [200, clientStamp + ',' + mockResponseClock]);
                 });
-            cli.sendClock((err, stamps) => {
-                let testEndStamp = Date.now();
-                expect(stamps.sent).to.be.at.least(testBeginStamp);
-                expect(stamps.received).to.be.at.most(testEndStamp);
-                expect(stamps.remote).to.be.equal(responseStamp);
+            cli.sendClock(testBeginStamp, (err, remoteClock) => {
+                expect(remoteClock).to.be.equal(mockResponseClock);
                 done(err);
             });
         });
 
         it('should raise an error upon invalid request', function (done) {
             let cli = Client('http://crazyinvalidurl123123fjfq.ug:9919');
-            cli.sendClock((err, stamps) => {
+            cli.sendClock(testBeginStamp, (err, remoteClock) => {
                 expect(err).to.be.an('error');
                 expect(err.code).to.be.equal('ENOTFOUND');
-                expect(stamps).to.be.undef;
+                expect(remoteClock).to.be.undef;
                 done();
             });
         });
@@ -63,9 +178,9 @@ describe('ClockClient', function () {
                 .get('/')
                 .socketDelay(100)
                 .reply(400);
-            cli.sendClock((err, stamps) => {
+            cli.sendClock(testBeginStamp, (err, remoteClock) => {
                 expect(err).to.be.an('error');
-                expect(stamps).to.be.undef;
+                expect(remoteClock).to.be.undef;
                 done();
             });
         });
@@ -76,12 +191,12 @@ describe('ClockClient', function () {
                 .get('/')
                 .socketDelay(100)
                 .reply(200, '123456' + ',' + Date.now());
-            cli.sendClock((err, stamps) => {
+            cli.sendClock(testBeginStamp, (err, remoteClock) => {
                 expect(err).to.be.an('error');
-                expect(stamps).to.be.undef;
+                expect(remoteClock).to.be.undef;
                 done();
             });
         });
-
     });
+
 });
